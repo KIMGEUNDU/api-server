154fd8b15afc899435a4d7200a50f15f60fe7dd3
KIMGEUNDU <vlkoklv@naver.com>
2023-12-11 10:20:36 +0900

    docs: 관리자 댓글 게시판 아이디 변경

--- api/samples/dbinit6.js
+++ api/samples/dbinit6.js
@@ -3706,7 +3706,7 @@ async function registReply() {
       createdAt: getTime(-3, -60 * 60 * 1),
       extra: {
         type: 'reviewComment',
-        boardId: 5,
+        boardId: 8,
       },
     },
     {
